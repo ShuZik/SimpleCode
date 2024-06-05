dc051eba7e98ba3b4e52eb4a0a7f91af5770cd87
ShuZik <shuzik@ShuZiks-MacBook-Pro.local>
2024-06-05 10:28:18 +0300

    [SC-01] - Apple Hello with Animation

---
+++ SimpleCode/SimpleCodeApp.swift
@@ -0,0 +1,18 @@
+//
+//  SimpleCodeApp.swift
+//  SimpleCode
+//
+//  Created by ShuZik on 10.05.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SimpleCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HelloView()
+        }
+    }
+}
